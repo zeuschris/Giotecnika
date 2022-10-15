3b17d4ed7759f2d0f82b97963ade35c47fd26269
Chris Montes <zeuschris123@gmail.com>
2022-10-15 20:31:57 -0300

    update cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,27 +5,43 @@ import './Cart.scss'
 
 export const Cart = () => {
     const {cart, cartTotal, emptyCart, removeItem} = useCartContext()
+
     return (
         <div className="container cart-container">
-            <h1>Tu Carrito</h1>
             {
-            cart.map((item) =>(
-                <div key={item.id}>
-                    <h3>Nombre: {item.nombre}</h3>
-                    <img src={item.imagen} alt={item.nombre}/>
-                    <p>Precio: ${item.precio}</p>
-                    <p>Envio: ${item.envio}</p>
-                    <small>Cantidad: {item.cantidad}</small>
-                    <div>
-                    <button onClick={() => removeItem(item.id)} className="btn btn-danger my-3"><AiFillDelete/></button>
-                    </div>
-                    <hr/>
-                </div>
-            ))
-        }
-        <h3>Total : ${cartTotal()}</h3>
-        <Link className="btn btn-primary" to='/checkout'>Terminar mi compra</Link>
-        <button onClick={emptyCart} className="btn btn-dark">Vaciar Carrito</button>
+                cart.length === 0
+
+                ?
+
+                <>
+                    <h2>Tu carrito esta vacio</h2>
+                    <Link to='/' className='btn btn-primary my-3'>Ir al inicio</Link>
+                </>
+
+                :
+                <>
+                    <h1>Tu Carrito</h1>
+                {
+                        cart.map((item) =>(
+                        <div key={item.id}>
+                        <h3>Nombre: {item.nombre}</h3>
+                        <img src={item.imagen} alt={item.nombre}/>
+                        <p>Precio: ${item.precio}</p>
+                        <p>Envio: ${item.envio}</p>
+                        <small>Cantidad: {item.cantidad}</small>
+                        <div>
+                        <button onClick={() => removeItem(item.id)} className="btn btn-danger my-3"><AiFillDelete/></button>
+                        </div>
+                        <hr/>
+        </div>
+                    ))
+                }
+
+                    <h3>Total : ${cartTotal()}</h3>
+                    <Link className="btn btn-primary me-2" to='/checkout'>Terminar mi compra</Link>
+                    <button onClick={emptyCart} className="btn btn-dark">Vaciar Carrito</button>
+                </>
+                }
         </div>
     )
 }
