ec42e855852d5cad48bf6b9b06efb3b196e74fb3
Chris Montes <zeuschris123@gmail.com>
2022-10-15 23:09:38 -0300

    Update itemsList style

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,21 +1,31 @@
 import { Link } from 'react-router-dom'
 import './Item.scss'
+import Carousel from 'react-bootstrap/Carousel';
 
 const Item = ({items}) => {
 
     return (
         <div className='items'>
-            <h1>{items.name}</h1>
-            <div className='items-card'>
-                <p>Descripción: {items.descript}</p>
-                <p>Precio: ${items.price}</p>
-                <img src={items.img} alt={items.name} className='image-products'></img>
-                <div className='container-lookme'>
-                <Link to={`/item/${items.id}`} className="btn btn-primary d-flex justify-content-center lookme">Ver más</Link>
-                </div>
-            </div>
+            <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={items.img}
+          alt={items.name}
+        />
+        <Carousel.Caption>
+          <h1>{items.name}</h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    <p>Descripción: {items.descript}</p>
+    <p>Precio: ${items.price}</p>
+    <div className='container-lookme'>
+    <Link to={`/item/${items.id}`} className="btn btn-primary d-flex justify-content-center lookme">Ver más</Link>
+    </div>
         </div>
     )
 }
 
 export default Item
+
